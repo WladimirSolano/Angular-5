9477b5615dfa2c7fc503076751de76dd258f3ac1
Wladimir Solano <wsolano@tech-group.cl>
2018-04-03 14:14:04 -0300

    Se premite agregar nuevas cartas al presionar la tecla enter y el input no este vacio

---
+++ getting-started-ng5/src/app/new-card-input/new-card-input.component.ts
@@ -0,0 +1,30 @@
+import {Component, EventEmitter, HostListener, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-new-card-input',
+  templateUrl: './new-card-input.component.html',
+  styleUrls: ['./new-card-input.component.scss'],
+  host: {'class': 'col-4'}
+})
+export class NewCardInputComponent implements OnInit {
+
+  public newCard: any = {text: ''};
+  @Output() onCardAdd = new EventEmitter<string>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.code === "Enter" && this.newCard.text.length > 0) {
+      this.addCard(this.newCard.text);
+    }
+  }
+
+  addCard(text) {
+    this.onCardAdd.emit(text);
+    this.newCard.text = '';
+  }
+
+}
